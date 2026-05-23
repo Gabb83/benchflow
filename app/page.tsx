c5a1f3e75d07c305d94d7199d034ca5e53f2d14f
Gabriel Evangelista <evangelistagabriel798@gmail.com>
2026-05-23 11:18:57 -0300

    feat: header básico para o projeto

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div>
+    <div className="bg-[#121212] text-white p-5">
+      <header className="flex flex-row items-center justify-between">
+        <div>
+          <h1 className="text-xl font-black tracking-tighter italic">BenchFlow</h1>
+        </div>
+        <div className="flex flex-row items-center justify-between gap-5">
+          <button>Executar</button>
+          <button>Exportar</button>
+          <p>MVP v1.0</p>
+        </div>
+      </header>
     </div>
   );
 }
